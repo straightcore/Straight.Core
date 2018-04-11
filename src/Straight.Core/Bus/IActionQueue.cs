0c2d9fc43a118907a477dc9e25083ab3f33558e1
Morgan Dupire <dupire.morgan@gmail.com>
2018-04-12 00:07:59 +0200

    update copyright year in all files

--- src/Straight.Core/Bus/IActionQueue.cs
+++ src/Straight.Core/Bus/IActionQueue.cs
@@ -2,7 +2,7 @@
 // Straight Compagny
 // Straight Core
 // ==============================================================================================================
-// ©2016 Straight Compagny. All rights reserved.
+// ©2018 Straight Compagny. All rights reserved.
 // Licensed under the MIT License (MIT); you may not use this file except in compliance
 // with the License. You may obtain have a last condition or last licence at https://github.com/straightcore/Straight.Core/blob/master
 // Unless required by applicable law or agreed to in writing, software distributed under the License is
