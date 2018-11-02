1bd40d3fd4ae7d4cb5b543230848a19839133d90
Morgan Dupire <dupire.morgan@gmail.com>
2018-11-02 23:42:20 +0100

    conventions applied

--- src/Straight.Core/Extensions/Guard/Guard.cs
+++ src/Straight.Core/Extensions/Guard/Guard.cs
@@ -17,18 +17,22 @@ using System.Text.RegularExpressions;
 
 namespace Straight.Core.Extensions.Guard
 {
-    public static class Guard
+    internal static class Guard
     {
         public static void CheckIfArgumentIsNull<T>(this T source, string name)
         {
             if (source == null)
+            {
                 throw new ArgumentNullException(name);
+            }
         }
 
         public static void CheckIfArgumentIsNullOrEmpty(this string source, string name)
         {
             if (string.IsNullOrEmpty(source))
+            {
                 throw new ArgumentNullException(name);
+            }
         }
 
         public static void CheckIfArgumentIsNullOrEmpty(this IEnumerable<object> source, string name)
@@ -36,13 +40,17 @@ namespace Straight.Core.Extensions.Guard
             var enumerable = source as object[] ?? source as IList<object> ?? source.ToList();
             enumerable.CheckIfArgumentIsNull(name);
             if (!enumerable.Any())
+            {
                 throw new ArgumentNullException(name);
+            }
         }
 
         public static void CheckRegexValidity(this Regex regex, string value, string name)
         {
             if (!regex.IsMatch(value))
+            {
                 throw new ArgumentException($"{name} format is not valid");
+            }
         }
     }
 }
